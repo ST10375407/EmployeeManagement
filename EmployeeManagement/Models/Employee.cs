3fdd7a8e688c91368d2a7841e427de72db155bcc
Vhuhwavho Nemandava <ST10375407@rcconnect.edu.za>
2024-10-22 07:09:11 +0000

    Add project files.

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee: UserActivity
+    {
+        public int Id { get; set; }
+        public string LectureName { get; set; }  // Made consistent with lowercase 'string'
+        public string LectureSurname { get; set; }
+        public string EmployeeNo { get; set; }
+
+        public string ContactDetails { get; set; }
+        public string Model { get; set; }  // Corrected property name "Modele" to "Model"
+
+        public string Program { get; set; }
+
+        public int NumberOfHours { get; set; }  // Corrected property name "numberOfHouhours" to "NumberOfHours"
+
+        public int HourlyRate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ EmployeeManagement/Models/UserActivity.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class UserActivity
+    {
+        public String? CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public String? ModifiedByID { get; set; }
+        public DateTime  ModifiedOn { get; set; }
+
+
+
+    }
+}
